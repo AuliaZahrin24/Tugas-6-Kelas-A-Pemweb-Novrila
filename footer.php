7649fefbd3f8d74e1894da44935aa7f390d62af1
Novrila Aulia Zahrin <novrilazahrin@gmail.com>
2025-04-07 08:14:32 +0700

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,18 @@
+<script src="home/js/jquery.min.js"></script>
+<script src="home/js/jquery-migrate-3.0.1.min.js"></script>
+<script src="home/js/popper.min.js"></script>
+<script src="home/js/bootstrap.min.js"></script>
+<script src="home/js/jquery.easing.1.3.js"></script>
+<script src="home/js/jquery.waypoints.min.js"></script>
+<script src="home/js/jquery.stellar.min.js"></script>
+<script src="home/js/owl.carousel.min.js"></script>
+<script src="home/js/jquery.magnific-popup.min.js"></script>
+<script src="home/js/jquery.animateNumber.min.js"></script>
+<script src="home/js/scrollax.min.js"></script>
+<script src="home/js/google-map.js"></script>
+<script src="home/js/main.js"></script>
+
+
+</body>
+
+</html>
